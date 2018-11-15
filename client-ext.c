4fb36249a4239258e0855a2d3a3a933d80a92fd3
Jan Fischer <jan.philip.fischer@geekinbusiness.de>
2018-11-15 11:06:42 +0100

    Updated source for client binaries.

---
+++ client-ext.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+struct mesg_buffer {
+	long mesg_type;
+	char mesg_text[100];
+}message;
+
+int main()
+{
+	key_t key;
+	int msgid;
+	//ftok to generate unique key
+	key = ftok("/home/jan/.uscripts/brghtservice/brightness-daemon.c", 65);
+	
+	msgid = msgget(key, 0666 | IPC_CREAT);
+	message.mesg_type = 1;
+
+	strncpy(message.mesg_text, "EXT", 3);
+
+	msgsnd(msgid, &message, sizeof(message), 0);
+
+	printf("Data sent is: %s \n", message.mesg_text);
+	return 0;
+}
